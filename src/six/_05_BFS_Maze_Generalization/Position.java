838c5ce11b5075ca8a20f63fcbfc726e8afcabf7
cheng <792702352@qq.com>
2018-03-19 12:13:00 +0800

    广度优先遍历生成迷宫

---
+++ src/six/_05_BFS_Maze_Generalization/Position.java
@@ -0,0 +1,25 @@
+package six._05_BFS_Maze_Generalization;
+
+/**
+ * 坐标
+ *
+ * @author cheng
+ *         2018/3/19 12:04
+ */
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
