4360ec07fe9da64d05255535aaea77ecddccf751
cheng <792702352@qq.com>
2018-03-19 12:12:24 +0800

    非递归的深度优先遍历生成迷宫

---
+++ src/six/_04_Non_Recursive_DFS_Maze_Generalization/AlgorithmVisualizer.java
@@ -0,0 +1,90 @@
+package six._04_Non_Recursive_DFS_Maze_Generalization;
+
+import templates.AlgorithmVisHelper;
+
+import java.awt.*;
+import java.util.Stack;
+
+/**
+ * 控制层
+ *
+ * @author cheng
+ *         2018/3/19 11:53
+ */
+public class AlgorithmVisualizer {
+
+    private static int blockSide = 6;
+    private static int DELAY = 5;
+
+    private static final int d[][] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+
+    /**
+     * 数据
+     */
+    private MazeData data;
+    /**
+     * 视图
+     */
+    private AlgorithmFrame frame;
+
+    public AlgorithmVisualizer(int N, int M) {
+
+        // 初始化数据
+        data = new MazeData(N, M);
+        int sceneHeight = data.N() * blockSide;
+        int sceneWidth = data.M() * blockSide;
+
+        // 初始化视图    将GUI放入事件派生队列中（java事件分发线程）
+        EventQueue.invokeLater(() -> {
+            frame = new AlgorithmFrame("Random Maze Generation Visualization", sceneWidth, sceneHeight);
+
+            new Thread(() -> run()).start();
+        });
+    }
+
+    /**
+     * 动画逻辑
+     */
+    private void run() {
+
+        setData(-1, -1);
+        Stack<Position> stack = new Stack<>();
+        Position first = new Position(data.getEntranceX(), data.getEntranceY() + 1);
+        stack.push(first);
+        data.visited[first.getX()][first.getY()] = true;
+
+        while (!stack.empty()) {
+            Position curPos = stack.pop();
+            for (int i = 0; i < 4; i++) {
+                int newX = curPos.getX() + d[i][0] * 2;
+                int newY = curPos.getY() + d[i][1] * 2;
+
+                if (data.inArea(newX, newY) && !data.visited[newX][newY]) {
+                    stack.push(new Position(newX, newY));
+                    data.visited[newX][newY] = true;
+                    setData(curPos.getX() + d[i][0], curPos.getY() + d[i][1]);
+                }
+            }
+        }
+
+        setData(-1, -1);
+    }
+
+    private void setData(int x, int y) {
+        if (data.inArea(x, y)) {
+            data.maze[x][y] = MazeData.ROAD;
+        }
+
+        frame.render(data);
+        AlgorithmVisHelper.pause(DELAY);
+    }
+
+    public static void main(String[] args) {
+
+        // 高
+        int N = 101;
+        // 宽
+        int M = 101;
+        AlgorithmVisualizer visualizer = new AlgorithmVisualizer(N, M);
+    }
+}
---
+++ src/six/_04_Non_Recursive_DFS_Maze_Generalization/Position.java
@@ -0,0 +1,25 @@
+package six._04_Non_Recursive_DFS_Maze_Generalization;
+
+/**
+ * 坐标
+ *
+ * @author cheng
+ *         2018/3/19 11:54
+ */
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
