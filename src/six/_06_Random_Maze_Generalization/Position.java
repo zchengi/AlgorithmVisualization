98ff1cf308c883bd6576a06a612e951ab6c00064
cheng <792702352@qq.com>
2018-03-19 13:50:47 +0800

    随机队列生成迷宫

---
+++ src/six/_06_Random_Maze_Generalization/Position.java
@@ -0,0 +1,25 @@
+package six._06_Random_Maze_Generalization;
+
+/**
+ * 坐标
+ *
+ * @author cheng
+ *         2018/3/19 13:38
+ */
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
