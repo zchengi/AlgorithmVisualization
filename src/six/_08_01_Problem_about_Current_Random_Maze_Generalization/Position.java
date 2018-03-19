c56f99d02349fc10064ac3b35bbb19ddd5aec43c
cheng <792702352@qq.com>
2018-03-19 14:37:01 +0800

    随机性较弱的迷宫生成与解迷宫

---
+++ src/six/_08_01_Problem_about_Current_Random_Maze_Generalization/Position.java
@@ -0,0 +1,25 @@
+package six._08_01_Problem_about_Current_Random_Maze_Generalization;
+
+/**
+ * 坐标
+ *
+ * @author cheng
+ *         2018/3/19 14:20
+ */
+public class Position {
+
+    private int x, y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
---
+++ src/six/_08_01_Problem_about_Current_Random_Maze_Generalization/RandomQueue.java
@@ -0,0 +1,46 @@
+package six._08_01_Problem_about_Current_Random_Maze_Generalization;
+
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
+/**
+ * 随机队列 实现
+ *
+ * @author cheng
+ *         2018/3/19 14:20
+ */
+public class RandomQueue<E> {
+
+    private ArrayList<E> queue;
+
+    public RandomQueue() {
+        queue = new ArrayList<>();
+    }
+
+    public void add(E e) {
+        queue.add(e);
+    }
+
+    public E remove() {
+
+        if (queue.size() == 0) {
+            throw new NoSuchElementException("There's no element to remove in Random Queue.");
+        }
+
+        int randIndex = (int) (Math.random() * queue.size());
+
+        E randElement = queue.get(randIndex);
+        queue.set(randIndex, queue.get(queue.size() - 1));
+        queue.remove(randElement);
+
+        return randElement;
+    }
+
+    public int size() {
+        return queue.size();
+    }
+
+    public boolean empty() {
+        return queue.size() == 0;
+    }
+}
