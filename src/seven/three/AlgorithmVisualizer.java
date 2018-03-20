6dbd23264eee8dccd788a489996192fe0f7fe923
cheng <792702352@qq.com>
2018-03-20 11:13:35 +0800

    随机生成雷区

---
+++ src/seven/three/AlgorithmVisualizer.java
@@ -0,0 +1,63 @@
+package seven.three;
+
+import templates.AlgorithmVisHelper;
+
+import java.awt.*;
+
+/**
+ * 控制层
+ *
+ * @author cheng
+ *         2018/3/20 10:53
+ */
+public class AlgorithmVisualizer {
+
+    private static int blockSide = 32;
+    private static int DELAY = 5;
+
+    /**
+     * 数据
+     */
+    private MineSweeperData data;
+    /**
+     * 视图
+     */
+    private AlgorithmFrame frame;
+
+    public AlgorithmVisualizer(int N, int M, int mineNumber) {
+
+        // 初始化数据
+        data = new MineSweeperData(N, M, mineNumber);
+        int sceneWidth = M * blockSide;
+        int sceneHeight = N * blockSide;
+
+        // 初始化视图    将GUI放入事件派生队列中（java事件分发线程）
+        EventQueue.invokeLater(() -> {
+            frame = new AlgorithmFrame("Mine sweeper", sceneWidth, sceneHeight);
+
+            new Thread(() -> run()).start();
+        });
+    }
+
+    /**
+     * 动画逻辑
+     */
+    private void run() {
+        setData();
+    }
+
+    private void setData() {
+        frame.render(data);
+        AlgorithmVisHelper.pause(DELAY);
+    }
+
+
+    public static void main(String[] args) {
+
+        int N = 20;
+        int M = 20;
+        int mineNumber = 50;
+
+        AlgorithmVisualizer visualizer = new AlgorithmVisualizer(N, M, mineNumber);
+    }
+}
---
+++ src/seven/three/MineSweeperData.java
@@ -0,0 +1,103 @@
+package seven.three;
+
+/**
+ * 扫雷数据层
+ *
+ * @author cheng
+ *         2018/3/20 10:53
+ */
+public class MineSweeperData {
+
+    public static final String blockImageURL = "src/seven/resources/block.png";
+    public static final String flagImageURL = "src/seven/resources/flag.png";
+    public static final String mineImageURL = "src/seven/resources/mine.png";
+
+    private int N, M;
+    private boolean[][] mines;
+
+    public MineSweeperData(int N, int M, int mineNumber) {
+
+        if (N <= 0 || M <= 0) {
+            throw new IllegalArgumentException("Mine sweeper size is invalid.");
+        }
+        if (mineNumber < 0 || mineNumber > N * M) {
+            throw new IllegalArgumentException("Mine number is larger than the size of mine sweeper board.");
+        }
+
+        this.N = N;
+        this.M = M;
+        mines = new boolean[N][M];
+
+        generateMines(mineNumber);
+    }
+
+    public static String numberImageURL(int num) {
+        if (num < 0 || num > 8) {
+            throw new IllegalArgumentException("No such a number image.");
+        }
+        return String.format("src/seven/resources/%d.png", num);
+    }
+
+    public boolean isMine(int x, int y) {
+        if (!inArea(x, y)) {
+            throw new IllegalArgumentException("Out of index in isMine function.");
+        }
+        return mines[x][y];
+    }
+
+    private void generateMines(int mineNumber) {
+
+        for (int i = 0; i < mineNumber; i++) {
+            int x = i / M;
+            int y = i % M;
+            mines[x][y] = true;
+        }
+
+        // int swapTime = 100;
+        // int swapTime = 1000;
+        // int swapTime = N * M;
+        for (int i = 0; i < mineNumber; i++) {
+            /*int x1 = (int) (Math.random() * N);
+            int y1 = (int) (Math.random() * M);*/
+
+            int x1 = i / M;
+            int y1 = i % M;
+
+            int x2 = (int) (Math.random() * N);
+            int y2 = (int) (Math.random() * M);
+
+            swap(x1, y1, x2, y2);
+        }
+
+
+        // 因为有while-true的循环，性能不能保证
+        /*for (int i = 0; i < mineNumber; i++) {
+            while (true) {
+                int x = (int) (Math.random() * N);
+                int y = (int) (Math.random() * M);
+                if (!mines[x][y]) {
+                    mines[x][y] = true;
+                    break;
+                }
+            }
+        }*/
+    }
+
+    private void swap(int x1, int y1, int x2, int y2) {
+        boolean temp = mines[x1][y1];
+        mines[x1][y1] = mines[x2][y2];
+        mines[x2][y2] = temp;
+    }
+
+    private boolean inArea(int x, int y) {
+        return x >= 0 && x < N && y >= 0 && y < M;
+    }
+
+    public int N() {
+        return N;
+    }
+
+    public int M() {
+        return M;
+    }
+}
