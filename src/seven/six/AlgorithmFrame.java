149ad52833f87407f015f3a62bc92b23474d2adb
cheng <792702352@qq.com>
2018-03-20 12:41:03 +0800

    扫雷算法的交互操作

---
+++ src/seven/six/AlgorithmFrame.java
@@ -0,0 +1,102 @@
+package seven.six;
+
+import templates.AlgorithmVisHelper;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * 视图层
+ *
+ * @author cheng
+ *         2018/3/20 11:45
+ */
+public class AlgorithmFrame extends JFrame {
+
+    private int canvasWidth;
+    private int canvasHeight;
+
+    private MineSweeperData data;
+
+    public AlgorithmFrame(String title, int canvasWidth, int canvasHeight) {
+        super(title);
+
+        this.canvasWidth = canvasWidth;
+        this.canvasHeight = canvasHeight;
+
+        AlgorithmCanvas canvas = new AlgorithmCanvas();
+        this.setContentPane(canvas);
+
+        this.setResizable(false);
+        pack();
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public AlgorithmFrame(String title) {
+        this(title, 640, 480);
+    }
+
+    public void render(MineSweeperData data) {
+        this.data = data;
+        repaint();
+    }
+
+    private class AlgorithmCanvas extends JPanel {
+        AlgorithmCanvas() {
+            super(true);
+        }
+
+        // 绘制组件 Graphics：绘制的上下文环境
+        @Override
+        public void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            Graphics2D graphics2D = (Graphics2D) g;
+
+            // 打开抗锯齿
+            RenderingHints hints = new RenderingHints(
+                    RenderingHints.KEY_ANTIALIASING,
+                    RenderingHints.VALUE_ANTIALIAS_ON);
+            // 设置运行质量优先
+            hints.put(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+            graphics2D.addRenderingHints(hints);
+
+            // 具体绘制
+            int w = canvasWidth / data.M();
+            int h = canvasHeight / data.N();
+            for (int i = 0; i < data.N(); i++) {
+                for (int j = 0; j < data.M(); j++) {
+
+                    if (data.open[i][j]) {
+                        if (data.isMine(i, j)) {
+                            AlgorithmVisHelper.putImage(graphics2D, j * w, i * h, MineSweeperData.mineImageURL);
+                        } else {
+                            AlgorithmVisHelper.putImage(graphics2D, j * w, i * h, MineSweeperData.numberImageURL(data.getNumber(i, j)));
+                        }
+                    } else {
+                        if (data.flags[i][j]) {
+                            AlgorithmVisHelper.putImage(graphics2D, j * w, i * h, MineSweeperData.flagImageURL);
+                        } else {
+                            AlgorithmVisHelper.putImage(graphics2D, j * w, i * h, MineSweeperData.blockImageURL);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        @Override
+        public Dimension getPreferredSize() {
+            return new Dimension(canvasWidth, canvasHeight);
+        }
+    }
+
+    public int getCanvasWidth() {
+        return canvasWidth;
+    }
+
+    public int getCanvasHeight() {
+        return canvasHeight;
+    }
+}
---
+++ src/seven/six/AlgorithmVisualizer.java
@@ -0,0 +1,106 @@
+package seven.six;
+
+import templates.AlgorithmVisHelper;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * 控制层
+ *
+ * @author cheng
+ *         2018/3/20 11:45
+ */
+public class AlgorithmVisualizer {
+
+    private static int blockSide = 32;
+    private static int DELAY = 5;
+
+    /**
+     * 数据
+     */
+    private MineSweeperData data;
+    /**
+     * 视图
+     */
+    private AlgorithmFrame frame;
+
+    public AlgorithmVisualizer(int N, int M, int mineNumber) {
+
+        // 初始化数据
+        data = new MineSweeperData(N, M, mineNumber);
+        int sceneWidth = M * blockSide;
+        int sceneHeight = N * blockSide;
+
+        // 初始化视图    将GUI放入事件派生队列中（java事件分发线程）
+        EventQueue.invokeLater(() -> {
+            frame = new AlgorithmFrame("Mine sweeper", sceneWidth, sceneHeight);
+
+            frame.addMouseListener(new AlgorithmMouseListener());
+
+            new Thread(() -> run()).start();
+        });
+    }
+
+    /**
+     * 动画逻辑
+     */
+    private void run() {
+        setData(false, -1, -1);
+    }
+
+    private void setData(boolean isLeftClicked, int x, int y) {
+
+        if (data.inArea(x, y)) {
+            if (isLeftClicked) {
+                if (data.isMine(x, y)) {
+                    // Game Over
+                    System.out.println("Game Over!");
+                }
+                data.open[x][y] = true;
+            } else {
+                data.flags[x][y] = !data.flags[x][y];
+            }
+        }
+
+        frame.render(data);
+        AlgorithmVisHelper.pause(DELAY);
+    }
+
+    private class AlgorithmMouseListener extends MouseAdapter {
+        @Override
+        public void mouseReleased(MouseEvent event) {
+
+            event.translatePoint(
+                    -(frame.getBounds().width - frame.getCanvasWidth()),
+                    -(frame.getBounds().height - frame.getCanvasHeight())
+            );
+
+            Point pos = event.getPoint();
+
+            int w = frame.getCanvasWidth() / data.M();
+            int h = frame.getCanvasHeight() / data.N();
+
+            int x = pos.y / h;
+            int y = pos.x / w;
+            // System.out.println(x + " " + y);
+
+            if (SwingUtilities.isLeftMouseButton(event)) {
+                setData(true, x, y);
+            } else {
+                setData(false, x, y);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int N = 20;
+        int M = 20;
+        int mineNumber = 50;
+
+        AlgorithmVisualizer visualizer = new AlgorithmVisualizer(N, M, mineNumber);
+    }
+}
